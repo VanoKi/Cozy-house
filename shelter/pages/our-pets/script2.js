a7ac5eeea0738c940e6488a996d014f4fc329254
VanoKi <kirsvano@gmail.com>
2023-04-11 00:11:28 +0300

    fix: js file at second page

--- shelter/pages/our-pets/script2.js
+++ shelter/pages/our-pets/script2.js
@@ -1,6 +1,19 @@
-const container = document.querySelector('.pets_cards')
+let container = document.querySelector('.pets_cards')
 let cards = Array.from(container.querySelectorAll('.card'))
 // console.log(cards);
 cards.sort(() => Math.random() - 0.5)
+// cards.forEach((i) => container.appendChild(i))
 
-cards.forEach((card) => container.appendChild(card))
+
+let allCards = []
+for (let i = 0; i < 6; i++) {
+  for (let card of cards.slice(i,).concat(cards.slice(0, i))){
+    allCards.push(card);
+  }
+}
+
+container.innerHTML = '';
+
+let mySlice = 8
+
+allCards.slice(0, mySlice).forEach((card) => container.appendChild(card))
