7ec9949872dc0192711e688f37fd81f766b28c99
VanoKi <kirsvano@gmail.com>
2023-04-13 16:25:47 +0300

    feat: add popup to the firs page

--- shelter/pages/our-pets/script2.js
+++ shelter/pages/our-pets/script2.js
@@ -18,42 +18,43 @@ let mySlice = 8
 
 allCards.slice(0, mySlice).forEach((card) => container.appendChild(card))
 
-//popup
+// //popup
 
-let popId = ''
-const cards2 = document.querySelectorAll('.card')
-cards2.forEach(card => {
-  card.addEventListener('click', () => {
-    const cardId = card.classList[1];
-           popId = document.getElementById(cardId)
-    popId.style.display = 'flex';
-    // console.log(body);
-    body.classList.add('no-scroll')
-  })
-})
+// let popId = ''
+// const cards2 = document.querySelectorAll('.card')
+// cards2.forEach(card => {
+//   card.addEventListener('click', () => {
+//     const cardId = card.classList[1];
+//            popId = document.getElementById(cardId)
+//     popId.style.display = 'flex';
+//     // console.log(body);
+//     body.classList.add('no-scroll')
+//   })
+// })
 
-const popup = document.querySelector('.popup_container')
-const popupCard = document.querySelector('.popup_card');
-const body = document.querySelector('body')
-const closePopUp = document.querySelector('.close')
+// const popup = document.querySelector('.popup_container')
+// const popupCard = document.querySelector('.popup_card');
+// const body = document.querySelector('body')
+// const closePopUp = document.querySelector('.close')
 
-const divEl = document.querySelectorAll('.popup_container');
-divEl.forEach(el => {
-  el.addEventListener('click', (event) =>{
-    if (!event.target.closest('.popup_card')){
-    el.style.display = 'none';
-    // console.log(body);
-    body.classList.remove('no-scroll')
-    }
-  })
-})
+// const divEl = document.querySelectorAll('.popup_container');
+// divEl.forEach(el => {
+//   el.addEventListener('click', (event) =>{
+//     if (!event.target.closest('.popup_card')){
+//     el.style.display = 'none';
+//     // console.log(body);
+//     body.classList.remove('no-scroll')
+//     }
+//   })
+// })
 
-const close = document.querySelectorAll('.close');
-close.forEach(closeBtn => {
-  closeBtn.addEventListener('click', () => {
-    const parentPopup = closeBtn.closest('.popup_container');
-    parentPopup.style.display = 'none';
-    // console.log(body);
-    body.classList.remove('no-scroll')
-  });
-});
+// const close = document.querySelectorAll('.close');
+// close.forEach(closeBtn => {
+//   closeBtn.addEventListener('click', () => {
+//     const parentPopup = closeBtn.closest('.popup_container');
+//     parentPopup.style.display = 'none';
+//     // console.log(body);
+//     body.classList.remove('no-scroll')
+//   });
+// });
+
