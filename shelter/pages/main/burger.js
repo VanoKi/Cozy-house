663535897862a5fb17efd44b09db01a07d3286e1
VanoKi <kirsvano@gmail.com>
2023-04-13 19:26:21 +0300

    feat: add close burger after click on links

--- shelter/pages/main/burger.js
+++ shelter/pages/main/burger.js
@@ -1,11 +1,20 @@
 const burgerIco = document.querySelector('.burger_ico')
 const burgerWrap = document.querySelector('.burger_wrap')
+const body2 = document.querySelector('body')
+
 
 burgerIco.addEventListener('click', () => {
-  burgerIco.classList.toggle('.rotate');
-  if (burgerWrap.style.display === 'none') {
-    burgerWrap.style.display = 'block';
-  } else {
-    burgerWrap.style.display = 'none'
-  }
+  burgerIco.classList.toggle('rotate');
+  burgerWrap.classList.toggle('hide');
+  body2.classList.toggle('no-scroll');
+})
+
+const link = document.querySelectorAll('a')
+
+link.forEach(a => {
+  a.addEventListener('click', () => {
+    burgerIco.classList.toggle('rotate');
+    burgerWrap.classList.toggle('hide');
+    body2.classList.toggle('no-scroll');
+  })
 })
